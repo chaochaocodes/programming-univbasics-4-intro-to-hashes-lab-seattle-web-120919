d1e6bc5982a2166d26df1bbf27b5bdf3012451f6
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-26 02:26:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def new_hash
   # return an empty hash
-  new_hash = Hash.new 
+  new_hash = Hash.new
 end
 
 def my_hash
@@ -39,7 +39,7 @@ def update_counting_hash(hash, key)
   # given a hash and a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-  if update_counting_hash[key] 
+  if update_counting_hash[key]
     update_counting_hash[key] += 1
   else update_counting_hash[key] = 1
 end
